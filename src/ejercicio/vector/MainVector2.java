99974454f362d86c3f7e36f8ed5273f28be53dc4
JhonnyNinabanda <jeninabandap@uce.edu.ec>
2021-08-03 19:46:03 -0500

    Taller

---
+++ src/ejercicio/vector/MainVector2.java
@@ -0,0 +1,50 @@
+package ejercicio.vector;
+
+import java.util.Scanner;
+
+public class MainVector2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 1. declare un vector X cuya longuitud se solicite al usuario, de tipo int
+		// y construya un programa que pida al usuario ingrese los valores para cada posicion
+		// y finalmente que se imprima los valores ingresados
+	
+		Scanner Lector = new Scanner(System.in);
+		
+		int N;
+		
+		System.out.print("Ingrese la longuitud del vector: ");
+
+		N = Lector.nextInt();
+		
+		int X [] = new int [N];
+		
+		int n;
+		
+		for (int i=0; i<N; i++) {
+			
+			System.out.println("Ingrese valor de la posicion: " +i);
+			
+			
+			n = Lector.nextInt();
+			
+			X[i]=n;
+			
+		}
+	
+		for (int i=0; i<3; i++) {
+			
+			System.out.println("El valor de la posicion: " +i);
+			System.out.println(X[i]);
+			
+		}
+	
+		
+		
+		
+
+	}
+
+}
