a5fe5570f4bae0b9de5d9a08b50c18320b099bbb
JhonnyNinabanda <jeninabandap@uce.edu.ec>
2021-08-25 19:43:27 -0500

    25/08/2021

---
+++ src/convenciones/nomenclatura/java/ImagenAnimada.java
@@ -0,0 +1,14 @@
+package convenciones.nomenclatura.java;		// DECLARACION DE UN PAQUETE EN NOMNECLATURA EN JAVA
+
+public class ImagenAnimada {				//  DECLARACION DE UNA CLASE EN NOMNECLATURA EN JAVA
+
+	static final int ANCHURA_MINIMA = 4;	// DECLARACION DE UNA CONSTANTE EN NOMNECLATURA EN JAVA
+	
+	 public interface FigurtaGrande			// DECLARACION DE UNA INTERFACE EN NOMNECLATURA EN JAVA
+     {
+         public void cogerFondo();			// DECLARACION DE UN METODO EN NOMNECLATURA EN JAVA
+         
+         float miAnchura = 0;				// DECLARACION DE UNA VARIABLE EN NOMENCLATURA JAVA
+	
+}
+}
