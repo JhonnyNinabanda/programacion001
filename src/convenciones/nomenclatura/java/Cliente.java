068373ebcf757f67650a7f853a97b4869479bb48
Jhonny Ninabanda <jeninabandap@uce.edu.ec>
2021-08-26 18:17:17 -0500

    Nomenclatura

---
+++ src/convenciones/nomenclatura/java/Cliente.java
@@ -0,0 +1,15 @@
+package convenciones.nomenclatura.java;		// DECLARACION DE UN PAQUETE EN NOMNECLATURA EN JAVA
+
+public class Cliente {						//  DECLARACION DE UNA CLASE EN NOMNECLATURA EN JAVA
+	
+	static final int CODIGO_POSTAL= 593;  	// DECLARACION DE UNA CONSTANTE EN NOMNECLATURA EN JAVA
+	
+	 public interface Personal				// DECLARACION DE UNA INTERFACE EN NOMNECLATURA EN JAVA
+     {
+         public void trabajar();			// DECLARACION DE UN METODO EN NOMNECLATURA EN JAVA
+         
+         float sueldo = 0;					// DECLARACION DE UNA VARIABLE EN NOMENCLATURA JAVA
+         
+     }
+
+}
