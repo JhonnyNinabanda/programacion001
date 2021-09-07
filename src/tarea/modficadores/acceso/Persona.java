2104e4aa34726a1b6d0da78b1b6e07974d1cfa66
Jhonny Ninabanda <jeninabandap@uce.edu.ec>
2021-09-07 01:40:06 -0500

    practicando

---
+++ src/tarea/modficadores/acceso/Persona.java
@@ -0,0 +1,27 @@
+package tarea.modficadores.acceso;
+
+public class Persona {
+	
+	public String Nombre;				// Variable PUBLIC
+	public String Apellido;				// Variable PUBLIC
+	public  String Cedula;				// Variable PUBLIC
+	private String Genero;				// Variable PRIVATE
+	private int Edad;					// Variable PRIVATE
+	protected int Sueldo;				// Variable PROTECTED
+	int NroEmpleado;					// Variable DEFAULT
+	
+	public void PuestoTrabajo () {		// Metodo PUBLIC
+		
+	}
+	
+	private void CuentaBancaria () {	// Metodo PRIVATE
+		
+	}
+	
+	protected void PagarSueldo () {		// Metodo PROTECTED
+		
+	}
+	
+
+
+}
